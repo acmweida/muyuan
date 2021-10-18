d6e7382fba93f1aa5c2384610e02413e7412f9c5
acmweida <2456910384@qq.com>
2021-10-18 22:57:42 +0800

    1

---
+++ muyuan-gateway/src/test/java/com/muyuan/gateway/MuyuanGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muyuan.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuyuanGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
