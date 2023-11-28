efb81e762b0f303e81a8ee908ac2e06b6c834a0e
acmweida <2456910384@qq.com>
2023-11-28 17:58:58 +0800

    feat #73 转换器调整

--- muyuan-auth/src/main/java/com/muyuan/auth/base/config/WebSecurityConfig.java
+++ muyuan-auth/src/main/java/com/muyuan/auth/base/config/WebSecurityConfig.java
@@ -5,32 +5,30 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
-import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
+import javax.sql.DataSource;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
@@ -42,13 +40,16 @@ import java.util.UUID;
 //@Import(value = {WebMvcConfig.class, OAuth2AuthorizationServerConfiguration.class})
 public class WebSecurityConfig {
 
+    @Resource
+    private DataSource dataSource;
+
     @Bean
     @Order(1)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
             throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(Customizer.withDefaults());	// Enable OpenID Connect 1.0
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class);	// Enable OpenID Connect 1.0
+
         http
                 // Redirect to the login page when not authenticated from the
                 // authorization endpoint
@@ -91,22 +92,17 @@ public class WebSecurityConfig {
         return new InMemoryUserDetailsManager(userDetails);
     }
 
-    @Bean
+        @Bean
     public RegisteredClientRepository registeredClientRepository() {
-        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("oidc-client")
-                .clientSecret("{noop}secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-                .postLogoutRedirectUri("http://127.0.0.1:8080/")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
 
-        return new InMemoryRegisteredClientRepository(oidcClient);
+//        clients.jdbc(dataSource).passwordEncoder(new BCryptPasswordEncoder() {
+//            @Override
+//            public String encode(CharSequence rawPassword) {
+//                return "{bcrypt}" + super.encode(rawPassword);
+//            }
+//        });
+
+        return new JdbcRegisteredClientRepository(new JdbcTemplate(dataSource));
     }
 
     @Bean
@@ -151,8 +147,7 @@ public class WebSecurityConfig {
 //    @Resource
 //    UserServiceImpl userDetailsService;
 //
-//    @Resource
-//    DataSource dataSource;
+
 //
 //    @Resource
 //    RedisTemplate redisTemplate;
@@ -238,18 +233,7 @@ public class WebSecurityConfig {
 //        return userDetailsService;
 //    }
 //
-//    @Bean
-//    public RegisteredClientRepository registeredClientRepository() {
-//
-////        clients.jdbc(dataSource).passwordEncoder(new BCryptPasswordEncoder() {
-////            @Override
-////            public String encode(CharSequence rawPassword) {
-////                return "{bcrypt}" + super.encode(rawPassword);
-////            }
-////        });
-//
-//        return new JdbcRegisteredClientRepository(new JdbcTemplate(dataSource));
-//    }
+
 //
 //    @Bean
 //    public JWKSource<SecurityContext> jwkSource() {
